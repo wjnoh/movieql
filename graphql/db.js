c90044f2c3f002de30c3967f08b18e9576cb4600
WOOJE <woojenoh@gmail.com>
2019-03-17 23:13:53 +0900

    Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,37 +1,44 @@
-export const people = [
+export let movies = [
   {
     id: 0,
-    name: "wooje",
-    age: 27,
-    gender: "male"
+    name: "Captain Marvel",
+    score: 3
   },
   {
     id: 1,
-    name: "hohyun",
-    age: 28,
-    gender: "male"
+    name: "Aqua Man",
+    score: 3
   },
   {
     id: 2,
-    name: "kyungchul",
-    age: 27,
-    gender: "male"
-  },
-  {
-    id: 3,
-    name: "jiwon",
-    age: 23,
-    gender: "female"
-  },
-  {
-    id: 4,
-    name: "hyunjoo",
-    age: 27,
-    gender: "female"
+    name: "Ant-Man and the Wasp",
+    score: 4
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === id);
-  return filteredPeople[0];
+  const filteredMovie = movies.filter(movie => movie.id === id);
+  return filteredMovie[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
