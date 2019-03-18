06e430343b39b2f9fd58827902124af0f72203a9
WOOJE <woojenoh@gmail.com>
2019-03-18 21:33:24 +0900

    Add movie Query

--- graphql/db.js
+++ graphql/db.js
@@ -1,10 +1,18 @@
 import fetch from "node-fetch";
 
-const MOVIE_URL = "https://yts.am/api/v2/list_movies.json?";
+const DETAIL_URL = "https://yts.am/api/v2/movie_details.json?";
+const LIST_URL = "https://yts.am/api/v2/list_movies.json?";
 const SUGGESTIONS_URL = "https://yts.am/api/v2/movie_suggestions.json?";
 
+export const getMovie = id => {
+  let REQUEST_URL = DETAIL_URL + `movie_id=${id}`;
+  return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movie);
+};
+
 export const getMovies = (limit, rating) => {
-  let REQUEST_URL = MOVIE_URL;
+  let REQUEST_URL = LIST_URL;
   if (limit > 0) {
     REQUEST_URL += `limit=${limit}`;
   }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,8 @@
-import { getMovies, getSuggestions } from "./db";
+import { getMovie, getMovies, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
+    movie: (_, { id }) => getMovie(id),
     movies: (_, { limit, rating }) => getMovies(limit, rating),
     suggestions: (_, { id }) => getSuggestions(id)
   }
